2fbd407a6b1cc95adb9ddd06ff617bbc879252cf
Glen <imaf7024@gmail.com>
2025-06-29 17:33:53 +0300

    keyShower 1.0

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+
+const testButton = document.querySelector('.test-button')
+testButton.addEventListener("click" , ()=> {
+    alert (testButton.innerHTML)
+})
+
 let newDiv = document.createElement("div")
 newDiv.innerHTML = "<h1>Это вставленный текст</h1>"
 newDiv.className = "newDiv"
 document.body.append(newDiv)
 
-const currentKey = '1'
-let pressedKeyShower = document.createElement("p")
-pressedKeyShower.innerHTML = "<h1> Текущая клавиша : " + currentKey + "</h1>"
-document.body.append(pressedKeyShower)
+let keyLog = document.createElement("p")
 
-// function keyShower(){
-//     addEventListener(ke)
-// }
+
+document.body.append(keyLog)
+keyLog.className = "key-log"
+
+document.addEventListener("keydown" , (e)=> {
+    if (e.key == "Shift" || e.key == "Control" || e.key == "Alt"){
+        return ''
+    } 
+
+    else if (e.key == "Backspace"){
+        keyLog.textContent
+    }
+    keyLog.textContent += `${e.key}`
+})
+
 
 //идея в том , чтобы сделать слушатель нажатых кнопок . типо чтоб пользователь видел ,
 //какую кнопку он клацнул. это все надо для того , чтоб 1)вспомнить как кодить на js , 2) разобраться
