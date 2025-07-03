1df4579460e487a2eed145c8af803a0322220851
Glen <imaf7024@gmail.com>
2025-07-03 22:04:39 +0300

    added "Spoiler" task description

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//0) вспоминаю, что такое append и alert + selector
 
 const testButton = document.querySelector('.test-button')
+const mainBox = document.querySelector('main')
 testButton.addEventListener("click" , ()=> {
     alert (testButton.innerHTML)
 })
@@ -7,12 +9,18 @@ testButton.addEventListener("click" , ()=> {
 let newDiv = document.createElement("div")
 newDiv.innerHTML = "<h1>Это вставленный текст</h1>"
 newDiv.className = "newDiv"
-document.body.append(newDiv)
+mainBox.append(newDiv)
+
+//==========================================================================================================
+
+//1)Ввод клавиш через eventListener keyDown (СДЕЛАНО)
+//идея в том , чтобы сделать слушатель нажатых кнопок . типо чтоб пользователь видел ,
+//какую кнопку он клацнул. это все надо для того , чтоб 1)вспомнить как кодить на js , 2) разобраться
+//как работать с кнопками через keyUp и keyDown
 
 let keyLog = document.createElement("p")
 
-
-document.body.append(keyLog)
+mainBox.append(keyLog)
 keyLog.className = "key-log"
 
 document.addEventListener("keydown" , (e)=> {
@@ -26,7 +34,33 @@ document.addEventListener("keydown" , (e)=> {
     keyLog.textContent += `${e.key}`
 })
 
+//==========================================================================================================
 
-//идея в том , чтобы сделать слушатель нажатых кнопок . типо чтоб пользователь видел ,
-//какую кнопку он клацнул. это все надо для того , чтоб 1)вспомнить как кодить на js , 2) разобраться
-//как работать с кнопками через keyUp и keyDown
+//2)Описание каждого програмного блока при наведении на него ("Spoiler")
+//идея в том , чтобы при наведении на любой блок (например, на блок из задачи 1), снизу появлялось "окошко"
+//с описанием сделаной работы.
+//Для полного кайфа стоит сделать это всё через цикл, а значения добавить через объект, где будет такая
+//цепочка - задача1 : описание1 , задача2 : описание2
+
+
+let newDivCounter = 0
+
+newDiv.addEventListener('mouseover' , ()=> {
+    let newDivDesc = document.createElement("div")
+    if (newDivCounter == 0) {
+        newDivDesc.innerHTML = '<div class = "block-description"><p>Это элемент, вставленный при помощи Java Script</p></div>'
+        newDiv.after(newDivDesc)
+        newDivCounter ++
+    }
+
+    else{
+        return
+    }
+})
+
+newDiv.addEventListener('mouseleave', ()=> {
+    newDivDesc.remove()
+})
+
+
+//==========================================================================================================
