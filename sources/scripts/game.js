d6cdb43ad40563e9fac1ba860509b6d632c3f444
Glen <imaf7024@gmail.com>
2025-08-11 04:29:57 +0300

    123

--- sources/scripts/game.js
+++ sources/scripts/game.js
@@ -1,16 +1,38 @@
-const inventoryDisplay = document.querySelector('.inventory-display')
-const inventory = {}
+const clickHandler = {
+    sayHi: function(){
+        alert('Hello')
+    },
 
-inventory.bullets ='0'
-inventory.bandages = '0'
-inventory.wood = '0'
-
-for (let itemName of Object.keys(inventory)){
-    console.log(itemName)
-    for (let itemCount of Object.entries(inventory))
-        console.log (itemName + itemCount)
-    // let item = document.createElement('p')
-    // item.textContent = itemNode
-    // inventoryDisplay.append(item)
+    sayBye: function(){
+        alert('Bye bye!')
+    }
 }
+
+function buttonCreator(buttonTextContent , executableAction){
+    const button = document.createElement('button');
+    button.textContent = buttonTextContent;
+    button.addEventListener('click' , executableAction)
+    return button
+}
+
+const buttonGrid = document.querySelector('.button-grid')
+
+buttonGrid.append(buttonCreator('Hello' , clickHandler.sayHi))
+buttonGrid.append(buttonCreator('ByeBye' , clickHandler.sayBye))
+
+const inventoryDisplay = document.querySelector('.inventory-display')
+const inventory = {
+    bandage : 0,
+    wood : 0,
+    steel : 0,
+    water : 0
+}
+
+for (let item of Object.keys(inventory)){
+    const itemNode = document.createElement('p')
+    itemNode.textContent = `${item} : ${inventory[item]}`
+    inventoryDisplay.append(itemNode)
+}
+
+// console.log(Object.values(inventory))
 
