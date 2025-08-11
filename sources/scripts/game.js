fecadc9b7c1c96bce3268d7417bbfe04a9e10ade
Glen <imaf7024@gmail.com>
2025-08-11 23:44:37 +0300

    JS Matrix 101 + game css
    работал с матрицами , понадобятся при создании карты игры . еще чут чут подправил стили на странице игры

--- sources/scripts/game.js
+++ sources/scripts/game.js
@@ -1,10 +1,11 @@
-const clickHandler = {
-    sayHi: function(){
-        alert('Hello')
-    },
+const buttonGrid = document.querySelector('.button-grid')
 
-    sayBye: function(){
-        alert('Bye bye!')
+const clickHandler = {
+    inventoryDisplayHandler : function(){
+        inventoryDisplay.classList.toggle('hidden')
+    },
+    itemSearch : function(){
+        Math.random()
     }
 }
 
@@ -15,17 +16,11 @@ function buttonCreator(buttonTextContent , executableAction){
     return button
 }
 
-const buttonGrid = document.querySelector('.button-grid')
-
-buttonGrid.append(buttonCreator('Hello' , clickHandler.sayHi))
-buttonGrid.append(buttonCreator('ByeBye' , clickHandler.sayBye))
+buttonGrid.append(buttonCreator('Инвентарь' , clickHandler.inventoryDisplayHandler))
 
 const inventoryDisplay = document.querySelector('.inventory-display')
 const inventory = {
-    bandage : 0,
-    wood : 0,
-    steel : 0,
-    water : 0
+    
 }
 
 for (let item of Object.keys(inventory)){
@@ -35,4 +30,64 @@ for (let item of Object.keys(inventory)){
 }
 
 // console.log(Object.values(inventory))
+
+const iterableObject = {};
+function objectCycleAppendTester(){
+    let k = 10
+    let i = 0;
+    while (i < 10 && k > 0){
+        iterableObject[`object${i}`] = k
+        i++
+        k--
+    }
+    console.log('Итоговое значение перечисленного объекта:')
+    console.log(iterableObject)
+}
+
+objectCycleAppendTester()
+
+
+console.log('-----------------------------------------')
+//==========================================================================================================
+
+function matrixBuilder(height = 0, width = 0){
+    const matrix = [];
+    while(height > 0){
+        const matrixRow = []
+        let rowWidth = width
+        while(rowWidth > 0){
+            matrixRow.push('')
+            rowWidth--
+        }
+        matrix.push(matrixRow)
+        height--
+    }
+
+    return matrix
+}
+
+function matrixSetter(matrixName, y , x , assignedValue){
+    (matrixName[y])[x] = assignedValue
+}
+
+function matrixSizeGetter(matrixName){
+    const y = matrixName.length;
+    const x = matrixName[0].length
+    return [y,x]
+}
+
+// coordLearner(1,0)
+
+const testMatrix = [
+    [1,2,3],
+    [4,5,6],
+]
+console.log(`Высота массива: ${testMatrix.length} рядов`)
+console.log(`Ширина массива: ${(testMatrix[0].length)} столбцов`)
+
+const newMatrix = matrixBuilder(6,3);
+matrixSetter(newMatrix, 0 , 1 , 99)
+
+console.log(newMatrix)
+console.log(matrixSizeGetter(newMatrix))
 
