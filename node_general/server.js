b80a6fc55dd7971e0ccfaa9999dfac9d132e6d90
Glen <imaf7024@gmail.com>
2025-09-04 23:55:50 +0300

    лилипутские шаги на втречу Node
    нет ну это просто пиздец как плохо . простите меня за фарм коммитов

--- node_general/server.js
+++ node_general/server.js
@@ -1,3 +1,4 @@
+// const chalk = require('chalk')
 const http = require('http')
 const hostname = '127.0.0.1'
 const port = 3000
@@ -6,4 +7,34 @@ const server = http.createServer((req , res) => {
     res.setHeader('Content-Type', 'text/plain')
     res.end('Hello Polytech\n')
 })
-server.listen(port, hostname)
+
+const fs = require('fs')
+fs.open('test.txt', 'w+', (err, fd) => {
+})
+
+try {
+const fd = fs.openSync('test.txt', 'w+')
+} catch (err) {
+console.error(err)
+}
+
+
+const startTime = new Date()
+const timeOptions = {
+    weekday : 'short',
+    year : 'numeric',
+    month : 'long',
+    day : 'numeric',
+    era : 'short',
+    hourCycle : 'h12',
+    hour : '2-digit',
+    minute : '2-digit',
+    second : '2-digit',
+    dayPeriod : 'short',
+    timeZoneName : 'shortOffset'
+}
+
+server.listen(port, hostname, ()=>{
+    console.log(`Сервер удачно запущен в ${startTime.toLocaleDateString('ru-RU' , timeOptions)}`)
+})
+// process.exit()
